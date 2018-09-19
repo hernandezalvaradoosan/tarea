910f2d629989d3b0e45a9abfd266e57c63d631f8
hernandezalvaradoosan <43357547+hernandezalvaradoosan@users.noreply.github.com>
2018-09-19 06:19:06 -0500

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("El primer numero es: 5 ");
+	scanf("%d", &a);
+	printf("El segundo numero es: 7 ");
+	c= a+b;
+	printf("El Resultado es: 10",c);
+	return 0;
+}
